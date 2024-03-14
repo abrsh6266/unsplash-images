4e52a8ee443621968c5fcc702fd23a80f58cf720
Abrham <abrsh6266@gmail.com>
2024-03-14 06:10:25 -0800

    dark theme functionality

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,22 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const appContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  return <appContext.Provider value={{}}>{children}</appContext.Provider>;
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    const newTheme = !isDarkTheme;
+    setIsDarkTheme(newTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", newTheme);
+    console.log(body);
+  };
+  return (
+    <appContext.Provider value={{ toggleDarkTheme, isDarkTheme }}>
+      {children}
+    </appContext.Provider>
+  );
 };
 export const useGlobalContext = () => {
   return useContext(appContext);

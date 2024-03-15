d773733cb175fbbab2ee7c2401ee8c4258880daa
Abrham <abrsh6266@gmail.com>
2024-03-15 04:19:36 -0800

    search functionality

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,7 +1,25 @@
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    console.log(searchValue);
+  };
   return (
-    <h2>SearchForm</h2>
-  )
-}
+    <section>
+      <h1 className="title">Unsplash images</h1>
+      <form action="" className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          placeholder="cat"
+          className="search-input"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
+};
 
-export default SearchForm
+export default SearchForm;

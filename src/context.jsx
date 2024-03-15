f5c16abf3310c6c5643b624f6cbe54a1fb1562bf
Abrham <abrsh6266@gmail.com>
2024-03-15 08:45:16 -0800

    adding user preference of dark theme in context

--- src/context.jsx
+++ src/context.jsx
@@ -1,17 +1,22 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const appContext = createContext();
+const getInitialDarkMode = () =>{
+  const prefersDarkMode = window.matchMedia('(prefers-color-schema:dark)').matches
+  return prefersDarkMode;
+}
 
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState("cat");
   const toggleDarkTheme = () => {
     const newTheme = !isDarkTheme;
     setIsDarkTheme(newTheme);
-    const body = document.querySelector("body");
-    body.classList.toggle("dark-theme", newTheme);
-    console.log(body);
   };
+  useEffect(()=>{
+    console.log(isDarkTheme)
+    document.body.classList.toggle('dark-theme',isDarkTheme)
+  },[isDarkTheme])
   return (
     <appContext.Provider
       value={{
